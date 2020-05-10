e85a64e725baa6c22244310df6e86cdc6c7786aa
Dionei J. Delangolo <dionei.jd@gmail.com>
2020-05-10 19:25:13 -0300

    Initial extesion screen

---
+++ src/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    
+
+    
+
+
+
+
+    document.querySelector('#btnRunReport').addEventListener('click', function(){
+
+    })
+
+
+
+    
+})
